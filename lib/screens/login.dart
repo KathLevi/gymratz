dfddc5cc950ea1d11eecbdea2ac7a62f08d2ed27
kathlevi <kathleen.levi@ceresti.com>
2019-01-06 16:08:28 -0800

    login page almost done

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -7,24 +7,177 @@ class LoginScreen extends StatefulWidget {
   }
 }
 
-class LoginScreenState extends State<LoginScreen> {
+class LoginScreenState extends State<LoginScreen> with WidgetsBindingObserver {
+  TextEditingController _emailCtrl;
+  TextEditingController _pwdCtrl;
+
+  FocusNode _userFocusNode;
+  FocusNode _passwordFocusNode;
+
+  bool _pwdHidden = true;
 
   @override
-    Widget build(BuildContext context) {
-      return Scaffold(
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _userFocusNode = FocusNode();
+    _passwordFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _userFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         resizeToAvoidBottomPadding: false,
         appBar: null,
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/mainImage.jpg'),
-                  fit: BoxFit.cover,
-                  alignment: FractionalOffset(0.6, 0.0))),
-          child: Column(children: <Widget>[
-            Text('GYMRATZ', style: TextStyle(color: Colors.white,),),
-          ],),),
-      ));
-    }
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/mainImage.jpg'),
+                    fit: BoxFit.cover,
+                    alignment: FractionalOffset(0.6, 0.0))),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 100.0),
+                  child: Text(
+                    'GYMRATZ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50.0,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        controller: _emailCtrl,
+                        keyboardType: TextInputType.emailAddress,
+                        textCapitalization: TextCapitalization.none,
+                        autocorrect: false,
+                        focusNode: _userFocusNode,
+                        onFieldSubmitted: (str) {
+                          _userFocusNode.unfocus();
+                          FocusScope.of(context)
+                              .requestFocus(_passwordFocusNode);
+                        },
+                        validator: (val) {
+                          return val.length < 10 ? "Email is not valid" : null;
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Username',
+                            labelStyle: TextStyle(color: Colors.white),
+                            enabledBorder: new UnderlineInputBorder(
+                                borderSide:
+                                    new BorderSide(color: Colors.white)),
+                            focusedBorder: new UnderlineInputBorder(
+                                borderSide:
+                                    new BorderSide(color: Colors.white))),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      Stack(
+                        children: <Widget>[
+                          TextFormField(
+                            controller: _pwdCtrl,
+                            obscureText: _pwdHidden,
+                            textCapitalization: TextCapitalization.none,
+                            autocorrect: false,
+                            focusNode: _passwordFocusNode,
+                            // onFieldSubmitted: onSubmitted,
+                            decoration: InputDecoration(
+                                labelText: 'Password',
+                                labelStyle: TextStyle(color: Colors.white),
+                                enabledBorder: new UnderlineInputBorder(
+                                    borderSide:
+                                        new BorderSide(color: Colors.white)),
+                                focusedBorder: new UnderlineInputBorder(
+                                    borderSide:
+                                        new BorderSide(color: Colors.white))),
+                            textInputAction: TextInputAction.go,
+                          ),
+                          Container(
+                            padding: const EdgeInsets.only(top: 15.0),
+                            alignment: FractionalOffset.bottomRight,
+                            child: IconButton(
+                                icon: Icon(
+                                  _pwdHidden
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _pwdHidden = !_pwdHidden;
+                                  });
+                                }),
+                          )
+                        ],
+                      ),
+                      Container(
+                        alignment: Alignment.bottomRight,
+                        child: InkWell(
+                          onTap: () {
+                            print('password');
+                          },
+                          child: Text('Forgot Password?',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        child: RaisedButton(
+                          onPressed: () {
+                            print('log in');
+                          },
+                          color: Theme.of(context).primaryColor,
+                          child: Text('Log In',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                      Container(
+                        child: InkWell(
+                            onTap: () {
+                              print('password');
+                            },
+                            child: Row(
+                              children: <Widget>[
+                                Text('Don\'t have an account? ',
+                                    style: TextStyle(color: Colors.white)),
+                                Text(' Sign Up!',
+                                    style: TextStyle(
+                                        color: Theme.of(context).accentColor)),
+                              ],
+                            )),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        child: RaisedButton(
+                          onPressed: () {
+                            print('log in');
+                          },
+                          color: Colors.grey,
+                          child: Text('Continue As Guest',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
 }
