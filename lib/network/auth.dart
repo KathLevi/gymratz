e41a6cbfb70af33b71693bf0e4e64e6a92655476
Michael Sorensen <webdesign@msorensen.me>
2019-01-10 14:59:18 -0800

    adding to - do notes

--- lib/network/auth.dart
+++ lib/network/auth.dart
@@ -14,5 +14,6 @@ Future handleSignIn(String email, String password) async {
 Future<String> handleRegister(String email, String password) async {
   FirebaseUser user = await _auth.createUserWithEmailAndPassword(
       email: email, password: password);
+  // after register we need to add a new user document to the User collection in firestore
   return user.uid;
 }
