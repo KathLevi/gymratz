1344b5ea1bfdea8f8666186bd4d617374800be38
kathlevi <kathleen.levi@ceresti.com>
2019-01-13 16:58:24 -0800

    finished app bar and navigation drawer

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:gymratz/screens/forgot_password.dart';
 import 'package:gymratz/screens/home.dart';
 import 'package:gymratz/screens/login.dart';
+import 'package:gymratz/screens/my_gyms.dart';
 import 'package:gymratz/screens/profile.dart';
 import 'package:gymratz/screens/register.dart';
+import 'package:gymratz/screens/search.dart';
+import 'package:gymratz/screens/settings.dart';
 
 final routes = {
   '/': (BuildContext context) => new LoginScreen(), // switch route back to /
   '/register': (BuildContext context) => new RegisterScreen(),
   '/home': (BuildContext context) => new HomeScreen(),
   '/forgotPassword': (BuildContext context) => new ForgotPasswordScreen(),
-  '/profile': (BuildContext context) => new ProfileScreen()
+  '/profile': (BuildContext context) => new ProfileScreen(),
+  '/myGyms': (BuildContext context) => new MyGymsScreen(),
+  '/search': (BuildContext context) => new SearchScreen(),
+  '/settings': (BuildContext context) => new SettingsScreen()
 };
