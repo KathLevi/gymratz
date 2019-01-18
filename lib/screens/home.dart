e2e50ed1c8797b46e53daae394bcb51d3ff2c10a
mikkal24 <voltx180@gmail.com>
2019-01-17 23:23:27 -0800

    styled the gym rows up a little bit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,25 +14,21 @@ class HomeScreenState extends State<HomeScreen> {
   // get gyms from the database
 
   _buildListItem(BuildContext context, document) {
-    return Card(
-        child: ListTile(
-            title: Row(children: [
-      Expanded(
-          flex: 1,
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-              child: Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          fit: BoxFit.fitHeight,
-                          image: new NetworkImage(document['logo'])))))),
-      Expanded(
-        flex: 3,
-        child: Column(
-          children: <Widget>[Text(document['name']), Text(document['address'])],
-        ),
-      )
-    ])));
+    return 
+    // Padding(
+        // padding: EdgeInsets.all(8.0),
+        // child:
+         Card(child:
+              ListTile(
+                leading: Image.network(document.data['logo'], width: 50.0, height: 50.0, fit: BoxFit.contain),
+                title: Text(document.data['name']),
+                subtitle: Text(document.data['address']),
+                trailing: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                  Icon(Icons.favorite),
+                  Icon(Icons.location_on)
+                ],),));
   }
 
   _makeGymColumn() {
@@ -40,11 +36,19 @@ class HomeScreenState extends State<HomeScreen> {
         stream: fsAPI.getAllGyms(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) return const Text('Loading...');
-          return ListView.builder(
-              itemExtent: 80.0,
-              itemCount: snapshot.data.documents.length,
-              itemBuilder: (context, index) =>
-                  _buildListItem(context, snapshot.data.documents[index]));
+          return Column(children: [
+            Text(
+              snapshot.data.documents.length.toString() + " results",
+              textAlign: TextAlign.left,
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemExtent: 80.0,
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) => 
+                      _buildListItem(context, snapshot.data.documents[index])),
+            )
+          ]);
         });
   }
 
