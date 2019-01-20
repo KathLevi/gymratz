3468cd5c98c6aad1c0b08f5aa5900c7d240a5deb
Mikkal24 <voltx180@gmail.com>
2019-01-20 12:54:28 -0800

    forgot semicolon

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -22,7 +22,6 @@ import 'package:gymratz/widgets/app_bar.dart';
 import 'package:gymratz/widgets/drawer.dart';
 import 'package:gymratz/main.dart';
 
-
 class ProfileScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -30,10 +29,11 @@ class ProfileScreen extends StatefulWidget {
   }
 }
 
-class ProfileScreenState extends State<ProfileScreen> with WidgetsBindingObserver {
+class ProfileScreenState extends State<ProfileScreen>
+    with WidgetsBindingObserver {
   var currentUser;
 
-      void checkForToken() {
+  void checkForToken() {
     authAPI.getAuthenticatedUser().then((user) {
       if (user != null) {
         if (!user.isAnonymous) {
@@ -50,11 +50,10 @@ class ProfileScreenState extends State<ProfileScreen> with WidgetsBindingObserve
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    checkForToken()
+    checkForToken();
     WidgetsBinding.instance.addObserver(this);
-    
   }
 
   @override
@@ -63,51 +62,48 @@ class ProfileScreenState extends State<ProfileScreen> with WidgetsBindingObserve
         appBar: appBar(context),
         drawer: drawerMenu(context, currentUser),
         body: SafeArea(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    color: Colors.black,
-                    child: Center(
+            child: Row(children: <Widget>[
+          Expanded(
+              child: Container(
+                  color: Colors.black,
+                  child: Center(
                       child: Column(
+                    children: <Widget>[
+                      CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              'https://thrillseekersanonymous.com//wp-content/uploads/2011/10/Climber_Icon-600.jpg'),
+                          radius: 35.0),
+                      Text(
+                          (currentUser != null)
+                              ? currentUser.displayName
+                              : "loading...",
+                          style: TextStyle(color: Colors.white)),
+                      Row(
                         children: <Widget>[
-                          CircleAvatar(
-                            backgroundImage: NetworkImage('https://thrillseekersanonymous.com//wp-content/uploads/2011/10/Climber_Icon-600.jpg'),
-                            radius: 35.0
-                          ),
-                          Text(
-                            (currentUser != null)?currentUser.displayName:"loading...",
-                            style: TextStyle(color: Colors.white)),
-                          Row(
-
-                            children: <Widget>[
-                            Expanded(child: Text(
-                              'My Profile',
-                              style: TextStyle(color: Colors.white),
-                              textAlign: TextAlign.center,
-                            )),
-                            Expanded(child: Text(
-                              'Friends',
-                              style: TextStyle(color: Colors.white),
-                              textAlign: TextAlign.center,
-                            )),
-                            Expanded(child: Text(
-                              'Comments',
-                              style: TextStyle(color: Colors.white),
-                              textAlign: TextAlign.center,
-                            ))
-                          ],)
+                          Expanded(
+                              child: Text(
+                            'My Profile',
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          )),
+                          Expanded(
+                              child: Text(
+                            'Friends',
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          )),
+                          Expanded(
+                              child: Text(
+                            'Comments',
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ))
                         ],
                       )
-                    )
-                  )
-                )
-              ]
-            )
-          )
-        );
+                    ],
+                  ))))
+        ])));
   }
 }
-
 
 // new NetworkImage("https://images.medicaldaily.com/sites/medicaldaily.com/files/styles/full_breakpoints_theme_medicaldaily_desktop_1x/public/2015/08/18/selfie-time.jpg")
