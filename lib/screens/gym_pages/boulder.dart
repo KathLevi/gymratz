ce8fd22d012fd3d355bf1518626753252866cb8b
Monkey <davidarnouk@gmail.com>
2019-08-03 14:13:21 -0700

    Laying the ground for the My Problems page, and fixed the Guest flash in the side bar (drawer)

--- lib/screens/gym_pages/boulder.dart
+++ lib/screens/gym_pages/boulder.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:gymratz/main.dart';
 import 'package:gymratz/network/data_types.dart';
 import 'package:gymratz/widgets/add_climb_button.dart';
+import 'package:gymratz/widgets/my_routes_column.dart';
 import 'package:gymratz/widgets/route_column.dart';
 
 class Boulder extends StatefulWidget {
   final Gym gym;
-  Boulder({Key key, @required this.gym}) : super(key: key);
+  final User user;
+  // Idea here is that this get Boulders given either a gym or a user
+  Boulder({Key key, this.gym, this.user}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -18,7 +21,7 @@ class BoulderState extends State<Boulder> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: <Widget>[
+      children: (widget.user == null) ? <Widget>[
         Column(
           children: <Widget>[
             Expanded(child: routeColumn(gym: widget.gym, boulder: true))
@@ -28,6 +31,13 @@ class BoulderState extends State<Boulder> with WidgetsBindingObserver {
         authAPI.user == null
             ? Container()
             : addClimbButton(gym: widget.gym, context: context, function: null)
+      ]
+      : <Widget>[
+        Column(
+          children: <Widget>[
+            Expanded(child: myRoutesColumn(user: widget.user, type: 'boulder'))
+          ],
+        ),
       ],
     );
   }
